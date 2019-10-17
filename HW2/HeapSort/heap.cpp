a54ca2e4e6db2612d5ac02f6768fc1f53ddc4a96
aschavez <aschavez@bu.edu>
2019-10-17 11:15:18 -0400

    HeapSort addition

--- HW2/HeapSort/heap.cpp
+++ HW2/HeapSort/heap.cpp
@@ -20,6 +20,7 @@ void CheckHeapOrder(int *Heap,int n);
 int  DeleteHeap(int index,int *Heap,int ntemp);
 void InsertHeap(int value ,int *Heap, int ntemp);
 void HeapSort(int *Heap, int n);
+void swap(int* a, int* b);
  
 int main(int argc, char *argv[]) 
 {
@@ -83,7 +84,9 @@ int main(int argc, char *argv[])
    // Try Deleting 5 
    for(int k = 0; k <5; k++)
      {
-       index = rand()%n + 1;
+       index = rand()%ntemp + 1;
+       cout <<"value to be deleted at index "<< index<< endl;
+       cout <<"ntemp "<< ntemp<< endl;
        value = DeleteHeap(index,Heap,ntemp); // Also reHeapfies 
        Heap[ntemp] = value;
        ntemp--;
@@ -92,9 +95,12 @@ int main(int argc, char *argv[])
    for(int k = 0; k <5; k++)
      {
        value = Heap[ntemp +1];
+       cout <<"value to be added  "<< value <<"at ntemp "<< ntemp<< endl;
        InsertHeap(value,Heap, ntemp); // Also reHeapfies
        ntemp++;
      }
+
+     cout <<"heapify complete"<<endl;
       CheckHeapOrder(Heap,n); // Is it still a  Heap?
 #endif
       
@@ -106,9 +112,9 @@ int main(int argc, char *argv[])
       
   OpCount = 0;
   start = chrono::steady_clock::now();
-   mergeSort(Heap, a_tmp,  1, n);
+   //mergeSort(Heap, a_tmp,  1, n);
    // REPLACE WITH HEAP SORT //
-   // HeapSort(Heap, n);
+   HeapSort(Heap, n);
   stop = chrono::steady_clock::now();
   difference_in_time = stop - start;
   difference_in_seconds_heap_sort = double(difference_in_time.count());
@@ -150,27 +156,95 @@ void mergeSort(int *a, int *a_tmp, int l, int r)
 /****************************************
 Provide funtions below
 ****************************************/
-
  void Heapify(int *Heap, int n)
  {
+   // Check if heap array is empty
+  if(n ==0)
+    return;
+
+  for (int i = 1; i < n; i++)  
+    { 
+        // Detect if child is greater than parent  (Max-heap property)
+        if (Heap[i] > Heap[(i - 1) / 2])  
+        { 
+            int j = i; 
+            // swap child and parent until child is smaller 
+            while (Heap[j] > Heap[(j - 1) / 2])  
+            { 
+                swap(Heap[j], Heap[(j - 1) / 2]); 
+                j = (j - 1) / 2; 
+            } 
+        } 
+    } 
  }
 
- void  CheckHeapOrder(int *Heap,int n)
-  {
-
-  }
+void  CheckHeapOrder(int *Heap,int n)
+{
+  // We can print the array Heap to check order
+  for (int i = 0; i < n; i++)
+      cout << Heap[i]<<" ";
+  cout << "End of Array" << endl;
+  
+}
 
 int  DeleteHeap(int index,int *Heap,int ntemp)
 {
   int save = Heap[index];
-
+  // Shift values from heap 
+  for(int i = index; i < ntemp-1; i++)
+    Heap[i]=Heap[i+1]; // copy elements and shift to the left
+  // Heapify new array
+  Heapify(Heap, ntemp);
   return  save;
 }
 void InsertHeap(int value ,int *Heap, int ntemp)
 {
-
+  // Insert Heap into the last index of array
+  Heap[ntemp] = value;
+  // Heapify new array
+  Heapify(Heap, ntemp);
 }
 void HeapSort(int *Heap, int n)
 {
-
+  /* Place Heap array in max-heap order just to be sure
+   that input is a heap*/
+  Heapify (Heap, n);
+  //Define looping variables
+  int j, index;
+  for (int i = n - 1; i > 0; i--) 
+  { 
+    swap(Heap[0], Heap[i]); 
+    //restart looping variables
+    j = 0;
+    index = 0;
+    while (index<i)
+    {
+      index = (2*j+1);
+      // Left child is smaller than right child
+      if(Heap[index]<Heap[index+1] && index < (i-1))
+      {
+      // move index to the right child  
+        index ++;
+      }
+      // New Parent is smaller than the child
+      if (Heap[j]<Heap[index] && index < i)
+      {
+      // Swap nodes
+        swap(Heap[j], Heap[index]);
+      }
+      j = index;
+    }  
+  }
+}
+/* sources:
+ - https://rosettacode.org/wiki/Sorting_algorithms/Heapsort
+ - http://www.mathcs.emory.edu/~cheung/Courses/171/Syllabus/9-BinTree/heap-sort2.html
+*/
+void swap(int* a, int* b)
+{
+  // put in swapCount
+    int temp;
+    temp = *a; 
+    *a = *b; 
+    *b = temp; 
 }
